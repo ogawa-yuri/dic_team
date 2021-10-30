1c4c41258a24bf7949d11dee044392770d43f3ff
yuri nomiyama <nomiyuri713@gmail.com>
2021-10-30 11:38:40 +0000

    編集権限変更

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    if @team.owner.id != current_user.id
+      redirect_to team_url, notice: I18n.t('views.messages.cannot_edit_other_member_except_leader')
+    end
+  end
 
   def create
     @team = Team.new(team_params)
